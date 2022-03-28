6875c4f879ff9e844ed1c527da854a83ca5cffee
Alexander Bodre <aboordonez@gmail.com>
2022-03-28 19:06:28 -0400

    Modelo e Interface

---
+++ BookApi/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookApi
+{
+    public class Book
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string title { get; set; }
+        [Required]
+        public string description { get; set; }
+        [Required]
+        public int pageCount { get; set; }
+        public string excerpt { get; set; }
+        public DateTime publishDate { get; set; }
+    }
+}
